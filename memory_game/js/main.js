9d53edabcb927b4211748cdee21eaddad0805afa
Talha Ikhlaq <talhaikhlaq@Talhas-MacBook-Pro.local>
2019-05-02 23:56:49 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -15,10 +36,10 @@ function checkForMatch() {
 function flipCard(cardId) {
 	flipCard(0);
 	flipCard(2);
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardsId]);
-	console.log(cards[cardId]);
-	console.log(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank)
+	cardsInPlay.push(cards[cardId.rank]);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[2])
 		alert("You found a match!");
@@ -26,9 +47,4 @@ function flipCard(cardId) {
 	} else {
 		alert("Sorry, try again.");
 	}
-	
-
-
-	
-	
 };
