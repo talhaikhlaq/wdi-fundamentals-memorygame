8c40fb74a10c224e2658b4cb7012b0ca08cf98f2
Talha Ikhlaq <talhaikhlaq@Talhas-MacBook-Pro.local>
2019-05-02 20:21:58 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,34 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push("cardTwo");
-console.log("User flipped king");
-cardsInPlay.length;
-if (cardsInPlay.length === 2) {};
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {alert("Sorry, try again.")
-};
 
+var cardsInPlay = [];
+
+cardsInPlay.length;
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
+}
+
+function flipCard(cardId) {
+	flipCard(0);
+	flipCard(2);
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardsId]);
+	console.log(cards[cardId]);
+	console.log(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[2])
+		alert("You found a match!");
+	checkForMatch();
+	} else {
+		alert("Sorry, try again.");
+	}
+	
+
+
+	
+	
+};
