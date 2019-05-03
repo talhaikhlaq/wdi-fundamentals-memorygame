6cd3efd08817f1118ad7cf258758b518dbc2d209
Talha Ikhlaq <talhaikhlaq@Talhas-MacBook-Pro.local>
2019-05-03 01:55:08 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,18 +33,31 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-	flipCard(0);
-	flipCard(2);
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank)
 	cardsInPlay.push(cards[cardId.rank]);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute("src", cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2])
 		alert("You found a match!");
 	checkForMatch();
 	} else {
 		alert("Sorry, try again.");
 	}
 };
+
+function createBoard() {
+	for (var i = 0;i < cards.length; i++) {
+	var cardElement = document.createElement("img");
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+	}	
+}
+
+createBoard();
+
+	
